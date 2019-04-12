bf2776ccda1575b4e95ae1dae57b43641397158c
zhangjunwei <zhangjunwei.0331@gmail.com>
2019-04-12 13:57:07 +0800

    添加java基础知识-数据类型篇

---
+++ src/main/java/com/zjw/datatype/Annotations.java
@@ -0,0 +1,19 @@
+package com.zjw.datatype;
+
+public class Annotations {
+
+    //作为整行注释，从//到本行结尾
+    int i;         //这之后作为注释直到结尾
+
+    /*
+    将一段包围起来作为注释
+    */
+
+   /**
+    * 1，包围整段
+    * 2，每行前面以“*”开头
+    * 3，可生产javadoc
+    * */
+
+
+}
